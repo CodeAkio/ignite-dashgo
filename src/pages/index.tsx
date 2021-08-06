944a95c191e8f60384adc9f855bde5c9ec87fdad
CodeAkio <siqueira_2004@hotmail.com>
2021-08-05 21:58:36 -0300

    Create sign in form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,46 @@
-export default function Home() {
+import { Button, Flex, Stack, } from '@chakra-ui/react'
+
+import { Input } from '../components/Form/Input'
+
+export default function SignIn() {
   return (
-    <h1>Dashboard</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={300}
+        bg="gray.800"
+        p={8}
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input
+            name="email"
+            type="email"
+            label="email"
+          />
+
+          <Input
+            name="password"
+            type="password"
+            label="password"
+          />
+
+          <Button
+            type="submit"
+            colorScheme="pink"
+            size="lg"
+          >
+            Entrar
+          </Button>
+        </Stack>
+      </Flex>
+    </Flex>
   )
 }
