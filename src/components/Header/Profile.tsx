5738ff895339bc143b660b0dac3468c88fc84f65
CodeAkio <siqueira_2004@hotmail.com>
2021-08-09 13:40:55 -0300

    Responsive Header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Code Akio</Text>
-        <Text color="gray.300" fontSize="small">akio@email.com</Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Code Akio</Text>
+          <Text color="gray.300" fontSize="small">
+            akio@email.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Code Akio" src="https://avatars.githubusercontent.com/u/15791870?v=4" />
     </Flex>
