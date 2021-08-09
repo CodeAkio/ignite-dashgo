d2f6b65309f55fa18bb82f860dd0615bd908d9ef
CodeAkio <siqueira_2004@hotmail.com>
2021-08-09 15:15:06 -0300

    Navigation

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,22 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue, IconButton } from "@chakra-ui/react";
+import Link from 'next/link'
+
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  Text,
+  useBreakpointValue,
+  IconButton
+} from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
@@ -22,15 +40,17 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">Usuários</Heading>
 
-            <Button
-              as="a"
-              size="sm"
-              fontSize="sm"
-              colorScheme="pink"
-              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
-            >
-              Adicionar
-            </Button>
+            <Link href="/users/create" passHref>
+              <Button
+                as="a"
+                size="sm"
+                fontSize="sm"
+                colorScheme="pink"
+                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+              >
+                Adicionar
+              </Button>
+            </Link>
           </Flex>
 
           <Table colorScheme="whiteAlpha">
